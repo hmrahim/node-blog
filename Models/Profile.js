da60b98e9f083b670064b36f06103167ba3a3d5c
hmrahim <h.m.rahimnet@gmail.com>
2023-12-03 12:19:17 +0600

    pforile and user model created

---
+++ Models/Profile.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const { schema } = require("./User");
+
+const profileSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      maxLength: 100,
+      trim: true,
+    },
+    bio: {
+      type: String,
+      maxLength: 500,
+      trim: true,
+    },
+    profilePic: String,
+    links: {
+      website: String,
+      facebook: String,
+      twitter: String,
+      github: String,
+    },
+    post: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    bookmark: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const Profile = model("Profile",profileSchema)
+
+module.exports = Profile
---
+++ Models/User.js
@@ -0,0 +1,30 @@
+const {Schema,model} = require("mongoose")
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        maxLength:30,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    profile:{
+        type:Schema.Types.ObjectId,
+        ref:"Profile"
+    }
+},{
+    timestamps:true
+})
+
+
+const User = model("User",userSchema)
+module.exports = User
